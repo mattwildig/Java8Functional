d424951e083e062e08194d35650c805d1d8b77b7
DebbieRoycroft <DebbieRoycroft@users.noreply.github.com>
2015-09-21 23:10:44 +0100

    New practical code added

---
+++ Java8Functional/practical/after/filesearch/FileSearch.java
@@ -0,0 +1,107 @@
+package filesearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileSearch {
+	//Regular expression to match word boundaries (space & punctuation)
+	private static final String WORD_REGEXP = "[- .:,;!?]+";
+	
+	/**
+	 * Count the number of words that match supplied word in supplied file
+	 * @param file			file to process
+	 * @param wordToMatch	word to look for
+	 * @return				number of matches
+	 */
+	public int countInstances(String file, String wordToMatch){
+		try (BufferedReader reader = Files.newBufferedReader(
+		        Paths.get(file), StandardCharsets.UTF_8)) {
+			final Pattern wordMatcher = Pattern.compile(WORD_REGEXP);
+			return (int) reader.lines()
+					.flatMap(wordMatcher::splitAsStream)
+					.filter(word -> Pattern.matches(wordToMatch, word))
+					.count();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	/**
+	 * Count the number of words in supplied file
+	 * @param file	file to process
+	 * @return		total number of words
+	 */
+	public int countWords(String file){
+		try (BufferedReader reader = Files.newBufferedReader(
+		        Paths.get(file), StandardCharsets.UTF_8)) {
+			final Pattern wordMatcher = Pattern.compile(WORD_REGEXP);
+			return (int) reader.lines()
+					.flatMap(wordMatcher::splitAsStream)
+					.count();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	/**
+	 * Count the number of different words in supplied file
+	 * uses case-insensitive match.
+	 * @param file	file to process
+	 * @return		total number of different words
+	 */
+	public int countDistinctWords(String file){
+		return process(file, 
+				(Stream<String> words)-> {
+					return (int)(words.map(String::toLowerCase)
+							.distinct()
+							.count());});
+	}
+
+	/**
+	 * Count the number of letters in the longest word in supplied file
+	 * uses case-insensitive match.
+	 * @param file	file to process
+	 * @return		length of longest word
+	 */	
+	public int longestWordLength(String file){
+		return process(file, 
+				(Stream<String> words)-> {
+					return words.collect(Collectors.summarizingInt(String::length))
+						.getMax();});
+	}
+	
+	//This method can be used to shorten the methods above, by extracting the repeating
+	//behaviour, and taking in a function to deal with the specialised behaviour
+	private int process(String file, Function<Stream<String>, Integer>wordProcessor ){
+		try (BufferedReader reader = Files.newBufferedReader(
+		        Paths.get(file), StandardCharsets.UTF_8)) {
+			final Pattern wordMatcher = Pattern.compile(WORD_REGEXP);
+			return wordProcessor.apply( reader.lines()
+					.flatMap(wordMatcher::splitAsStream));
+	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	public static void main(String args[]){
+		FileSearch fs = new FileSearch();
+		System.out.println("number of words: " + fs.countWords("Ode.txt"));
+		System.out.println("number of 'thy': " + fs.countInstances("Ode.txt", "thy"));
+		System.out.println("number of distinct words: " + fs.countDistinctWords("Ode.txt"));
+		System.out.println("length of longest word: " + fs.longestWordLength("Ode.txt"));
+	}
+}
---
+++ Java8Functional/practical/before/filesearch/FileSearch.java
@@ -0,0 +1,138 @@
+package filesearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class FileSearch {
+	//Regular expression to match word boundaries (space & punctuation)
+	private static final String WORD_REGEXP = "[- .:,;!?]+";
+	
+	/**
+	 * Count the number of words that match supplied word in supplied file
+	 * @param file			file to process
+	 * @param wordToMatch	word to look for
+	 * @return				number of matches
+	 */
+	public int countInstances(String file, String wordToMatch){
+		int count = 0;
+		final Pattern wordMatcher = Pattern.compile(WORD_REGEXP);
+		try (BufferedReader reader = Files.newBufferedReader(
+		        Paths.get(file), StandardCharsets.UTF_8)) {
+			String line = reader.readLine();
+			while (line != null){
+				String[] words = wordMatcher.split(line);
+				for( String word : words){
+					if (word.equals(wordToMatch)){
+						++count;
+					}
+				}
+				line = reader.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+	
+	/**
+	 * Count the number of words in supplied file
+	 * @param file	file to process
+	 * @return		total number of words
+	 */
+	public int countWords(String file){
+		int count = 0;
+		final Pattern wordMatcher = Pattern.compile(WORD_REGEXP);
+		try (BufferedReader reader = Files.newBufferedReader(
+		        Paths.get(file), StandardCharsets.UTF_8)) {
+			String line = reader.readLine();
+			while (line != null){
+				String[] words = wordMatcher.split(line);
+				for( String word : words){
+					//Make sure we only count words, not blank lines
+					if(!word.isEmpty()){
+						++ count;
+					}
+				}
+				line = reader.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	/**
+	 * Count the number of different words in supplied file
+	 * uses case-insensitive match.
+	 * @param file	file to process
+	 * @return		total number of different words
+	 */
+	public int countDistinctWords(String file){
+		int count = 0;
+		final Pattern wordMatcher = Pattern.compile(WORD_REGEXP);
+		try (BufferedReader reader = Files.newBufferedReader(
+		        Paths.get(file), StandardCharsets.UTF_8)) {
+			Set<String> distinctWords = new HashSet<>();
+			String line = reader.readLine();
+			while (line != null){
+				String[] words = wordMatcher.split(line);
+				for( String word : words){
+					//Make sure we only count words, not blank lines
+					if(!word.isEmpty()){
+						distinctWords.add(word.toLowerCase());
+					}
+				}
+				line = reader.readLine();
+			}
+			count = distinctWords.size();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	/**
+	 * Count the number of letters in the longest word in supplied file
+	 * uses case-insensitive match.
+	 * @param file	file to process
+	 * @return		length of longest word
+	 */
+	public int longestWordLength(String file){
+		int maxLength = 0;
+		final Pattern wordMatcher = Pattern.compile(WORD_REGEXP);
+		try (BufferedReader reader = Files.newBufferedReader(
+		        Paths.get(file), StandardCharsets.UTF_8)) {
+			String line = reader.readLine();
+			while (line != null){
+				String[] words = wordMatcher.split(line);
+				for( String word : words){
+					if(word.length() > maxLength){
+						maxLength = word.length();
+					}
+				}
+				line = reader.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return maxLength;
+	}
+
+	public static void main(String args[]){
+		FileSearch fs = new FileSearch();
+		System.out.println("number of words: " + fs.countWords("Ode.txt"));
+		System.out.println("number of 'thy': " + fs.countInstances("Ode.txt", "thy"));
+		System.out.println("number of distinct words: " + fs.countDistinctWords("Ode.txt"));
+		System.out.println("length of longest word: " + fs.longestWordLength("Ode.txt"));
+	}
+}
