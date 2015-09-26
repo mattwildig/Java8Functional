7e20ddbbb50b74159117d55d8e7f30f974a7e374
Matt Wildig <matt@mattwildig.co.uk>
2015-09-26 03:26:06 +0100

    Using import static in strategy

--- Java8Functional/practical/before/strategy/Main.java
+++ Java8Functional/practical/before/strategy/Main.java
@@ -3,14 +3,16 @@ package strategy;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.joining;
+
 public class Main {
 	public static void main(String args[]){
 		StrategyPattern test1 = new StrategyPattern(
-			list -> list.collect(Collectors.joining(","))
+			list -> list.collect(joining(","))
 		);
 
 		StrategyPattern test2 = new StrategyPattern(
-			list -> list.sorted().collect(Collectors.joining("\n", "", "\n"))
+			list -> list.sorted().collect(joining("\n", "", "\n"))
 		);
 		
 		test1.doStuff();
